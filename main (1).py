1d80721515d697db43da0f23893b5e2d863939b6
Sasirega30 <144321903+Sasirega30@users.noreply.github.com>
2023-09-15 12:58:23 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,15 +1,27 @@
-def recur_factorial(n):
-   if n == 1:
-       return n
-   else:
-       return n*recur_factorial(n-1)
+class BankAccount:
+    def __init__(self, acc_number, acc_holder_name, initial_balance=0):
+        self.__account_number = acc_number
+        self.__account_holder_name = acc_holder_name
+        self.__account_balance = initial_balance
 
-num = 7
+    def deposit(self, amount):
+        self.__account_balance += amount
 
-# check if the number is negative
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
-else:
-   print("The factorial of", num, "is", recur_factorial(num))
+    def withdraw(self, amount):
+        if amount <= self.__account_balance:
+            self.__account_balance -= amount
+        else:
+            print("Insufficient funds")
+
+    def display_balance(self):
+        return f"Account Balance: ${self.__account_balance}"
+
+# Create an instance of BankAccount
+account1 = BankAccount("12345", "John Doe", 1000)
+
+# Test deposit and withdrawal functionality
+account1.deposit(500)
+account1.withdraw(200)
+
+# Display account balance
+print(account1.display_balance())
--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-year=int(input("Enter year:"))
-# Leap Year Check
-if year % 4 == 0 and year % 100 != 0:
-    print(year, "is a Leap Year")
-elif year % 100 == 0:
-    print(year, "is not a Leap Year")
-elif year % 400 ==0:
-    print(year, "is a Leap Year")
-else:
-    print(year, "is not a Leap Year")
+class Player:
+    def play(self):
+        print("The player is playing cricket.")
+
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
+
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
+
+# Creating objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+
+# Calling the play() method for each object
+batsman.play()
+bowler.play()

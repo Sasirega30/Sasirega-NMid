1307dc69071bb3923e7274454cc19c9288e32a88
Sasirega30 <144321903+Sasirega30@users.noreply.github.com>
2023-09-27 14:32:37 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
-class Player:
-    def play(self):
-        print("The player is playing cricket.")
+class Student:
+    def __init__(self, name, roll_number, cgpa):
+        self.name = name
+        self.roll_number = roll_number
+        self.cgpa = cgpa
 
-class Batsman(Player):
-    def play(self):
-        print("The batsman is batting.")
+    def __str__(self):
+        return f"{self.name} ({self.roll_number}): {self.cgpa}"
 
-class Bowler(Player):
-    def play(self):
-        print("The bowler is bowling.")
+def sort_students(student_list):
+    sorted_students = sorted(student_list, key=lambda x: x.cgpa, reverse=True)
+    return sorted_students
 
-# Creating objects of Batsman and Bowler classes
-batsman = Batsman()
-bowler = Bowler()
+# Example usage:
+student1 = Student("Alice", "001", 3.8)
+student2 = Student("Bob", "002", 3.5)
+student3 = Student("Charlie", "003", 3.9)
+student4 = Student("David", "004", 3.7)
 
-# Calling the play() method for each object
-batsman.play()
-bowler.play()
+students = [student1, student2, student3, student4]
+
+sorted_students = sort_students(students)
+
+for student in sorted_students:
+    print(student)
